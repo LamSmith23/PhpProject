ad045da9f200bc479c2bddffcb2de5277e5ed30c
Lamont Smith <lsmith@techtonicgroup.com>
2017-08-21 16:40:07 -0600

    added the global wordpress object to communicate with my database table... data from my database is now displaying on my page!!!

--- wp-admin/index2.php
+++ wp-admin/index2.php
@@ -9,7 +9,7 @@
 <body>
     <div class="container">
             <div class="row">
-                <h3>PHP CRUD Grid</h3>
+                <h3 style="text-align:center; color:blue;">PHP CRUD Grid | Golden State Warriors</h3>
             </div>
             <div class="row">
                 <table class="table table-striped table-bordered">
@@ -18,14 +18,17 @@
                       <th> ID </th>
                       <th>First Name</th>
                       <th>Last Name</th>
-                      <th>Email</th>
+                      <th>Position</th>
                     </tr>
                   </thead>
                   <tbody>
                   <?php
                    include 'database.php';
+                   global $wpdb;
+
+
                    $pdo = Database::connect();
-                   $sql = 'SELECT * FROM  wp_roster ORDER BY id DESC';
+                   $sql = 'SELECT * FROM  wp_roster ORDER BY id ASC';
                    foreach ($pdo->query($sql) as $row) {
                             echo '<tr>';
                             echo '<td>'. $row['ID'] . '</td>';
