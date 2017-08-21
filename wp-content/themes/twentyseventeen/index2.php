b2a6a8166b8771dd595e4ea4e2ce7678dea8957c
Lamont Smith <lsmith@techtonicgroup.com>
2017-08-21 15:14:50 -0600

    created dabase table , created basic hmtl page to show database results

---
+++ wp-content/themes/twentyseventeen/index2.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <link   href="css/bootstrap.min.css" rel="stylesheet">
+    <script src="js/bootstrap.min.js"></script>
+</head>
+
+<body>
+    <div class="container">
+            <div class="row">
+                <h3>PHP CRUD Grid</h3>
+            </div>
+            <div class="row">
+                <table class="table table-striped table-bordered">
+                  <thead>
+                    <tr>
+                      <th> ID </th>
+                      <th>First Name</th>
+                      <th>Last Name</th>
+                      <th>Email</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                  <?php
+                   include 'database.php';
+                   $pdo = Database::connect();
+                   $sql = 'SELECT * FROM wp_roster ORDER BY id DESC';
+                   foreach ($pdo->query($sql) as $row) {
+                            echo '<tr>';
+                            echo '<td>'. $row['ID'] . '</td>';
+                            echo '<td>'. $row['First Name'] . '</td>';
+                            echo '<td>'. $row['Last Name'] . '</td>';
+                            echo '<td>'. $row['Position'] . '</td>';
+                            echo '</tr>';
+                   }
+                   Database::disconnect();
+                  ?>
+                  </tbody>
+            </table>
+        </div>
+    </div> <!-- /container -->
+  </body>
+</html>
